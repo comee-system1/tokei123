f25b846f1ee7b8eba5d5dd7ee7a83f929720e8a6
comee-system1 <system@comee.jp>
2022-07-14 01:08:39 +0900

    0714

--- frontend/sihmain/frontend/src/views/ServiceTeikyo.vue
+++ frontend/sihmain/frontend/src/views/ServiceTeikyo.vue
@@ -378,8 +378,6 @@ export default {
       flexGrid: '',
       selected: '',
       select_mandatory: false,
-      reseType: ['', '〇'],
-      takinouJigyo: ['', '〇', 'gray'],
       tabMenus: [
         { href: '#basic', text: '基本情報' },
         { href: '#sisetu', text: '施設体制' },
@@ -402,7 +400,7 @@ export default {
     this.handleResize;
   },
   created() {
-    this.getData();
+    // this.getData();
     this.getDataTeisai();
 
     window.addEventListener('resize', this.handleResize);
@@ -421,6 +419,9 @@ export default {
      * 表示切替
      */
     onToggleDisplay() {
+      if (this.tab != 'sisetu') {
+        return false;
+      }
       this.isVisible1 = false;
       this.isVisible2 = false;
       this.isVisible3 = false;
@@ -429,6 +430,9 @@ export default {
 
       // 一旦データの保持
       if (this.toggle_display == 0) {
+        if (this.serviceDataMain.length < 1) {
+          return false;
+        }
         // 施設種類・定員
         this.isVisible2 = true;
         this.isVisible23 = true;
@@ -439,6 +443,9 @@ export default {
         this.serviceData = this.serviceDataMain;
         this.allData = this.serviceDataMain;
       } else {
+        if (this.serviceDataTeisai.length < 1) {
+          return false;
+        }
         // 体制加算
         this.isVisible3 = true;
         this.isVisible23 = true;
@@ -455,13 +462,18 @@ export default {
      * 子コンポーネントtabmenublueで選択した値を取得
      */
     parent_tab_menu(args) {
-      this.tab = args.selectTab;
+      if (this.serviceDataMain && this.serviceDataMain.length < 1) {
+        return false;
+      }
+
+      if (args) {
+        this.tab = args.selectTab;
+      }
       this.isVisible1 = false;
       this.isVisible2 = false;
       this.isVisible3 = false;
       this.isVisible12 = false;
       this.isVisible23 = false;
-
       if (this.tab == 'basic') {
         this.isVisible1 = true;
         this.isVisible12 = true;
@@ -472,6 +484,7 @@ export default {
         this.isVisible12 = true;
         this.isVisible23 = true;
       }
+
       this.serviceData = this.serviceDataMain;
       this.allData = this.serviceDataMain;
 
@@ -502,14 +515,17 @@ export default {
      */
     onToggleEnable() {
       let array = [];
-      for (let i = 0; i < this.allData.length; i++) {
-        if (
-          (this.toggle_enable === 0 && this.allData[i].enabled === true) ||
-          (this.toggle_enable === 1 && this.allData[i].enabled === false)
-        ) {
-          array.push(this.allData[i]);
+      if (this.allData && this.allData.length > 0) {
+        for (let i = 0; i < this.allData.length; i++) {
+          if (
+            (this.toggle_enable === 0 && this.allData[i].enabled === true) ||
+            (this.toggle_enable === 1 && this.allData[i].enabled === false)
+          ) {
+            array.push(this.allData[i]);
+          }
         }
       }
+
       this.serviceData = [];
       this.serviceData = array;
       this.flexGrid.itemsSource = [];
@@ -520,7 +536,7 @@ export default {
      */
     toggleChangeSort() {
       let serviceData = this.serviceData;
-      if (this.toggle_sort === 0) {
+      if (this.toggle_sort === 0 && serviceData) {
         // コード順に並び替え
         serviceData.sort((a, b) => {
           if (a.code < b.code) {
@@ -532,7 +548,7 @@ export default {
           return 0;
         });
       }
-      if (this.toggle_sort === 1) {
+      if (this.toggle_sort === 1 && serviceData) {
         // 事業所番号順に並び替え
         serviceData.sort((a, b) => {
           if (a.jigyosyoBango < b.jigyosyoBango) {
@@ -550,6 +566,11 @@ export default {
     },
     onInitialized(flexGrid) {
       this.flexGrid = flexGrid;
+      this.getData().then((result) => {
+        this.serviceDataMain = result;
+        this.allDataMain = result;
+        this.parent_tab_menu();
+      });
       // ヘッダセル
       this.createHeader(flexGrid);
       // フォーマット
@@ -584,7 +605,6 @@ export default {
     },
     onItemsSourceChanged(flexGrid) {
       flexGrid.select(-1, -1);
-
       // ヘッダセルのマージ
       this.createHeaderMerge(flexGrid);
     },
@@ -602,6 +622,12 @@ export default {
           }
           e.cell.style.backgroundColor = sysConst.COLOR.lightYellow;
 
+          // レセ請求代表
+          if (e.col == 10) {
+            e.cell.style.textAlign = 'center';
+            e.cell.style.justifyContent = 'center';
+            e.cell.style.alignItems = 'center';
+          }
           if (e.col == 14) {
             if (e.panel.rows[e.row].dataItem.takinouJigyo == 'gray') {
               e.cell.style.backgroundColor = sysConst.COLOR.lightGray;
@@ -785,7 +811,6 @@ export default {
      */
     createMergeArray(groupData) {
       let array = [];
-
       for (let i = 0; i < groupData.length; i++) {
         array.push({
           row: i,
@@ -931,352 +956,13 @@ export default {
     },
     async getData() {
       let serviceData = [];
-      /*
-      serviceData.push(
-        {
-          code: '1001',
-          serviceJigyosyoMei: '障害者支援施設 ひまわり園',
-          ryakusyo: '生活ひまわり園',
-          serviceMeisyoCode: '22',
-          serviceMeisyo: '生活介護',
-          jigyosyoBango: '1121000011',
-          post1: '001',
-          post2: '0012',
-          jyusyo: '●●市××町1-1-1',
-          tel: '',
-          tel1: '03',
-          tel2: '1111',
-          tel3: '2222',
-          fax: '',
-          fax1: '03',
-          fax2: '1111',
-          fax3: '2222',
-          mail: '',
-          rese: 1,
-          enabled: true,
-          seikyuDaihyo: 0,
-          seikyuDaihyoDisp: '',
-          tekiouStartDate: '20220304',
-          kyutikubun: 1,
-          takinouJigyo: 0,
-          sisetsusyurui: '',
-          teiin: '21人～40人',
-          takinougataNado: '',
-          jinin: 'I型(17:1)',
-          jigyosyoku: '指定',
-          tujyo: '公立',
-          kasan: 'なし',
-          jigyojissi: '併設',
-          taisyo: '',
-        },
-        {
-          code: '1002',
-          serviceJigyosyoMei: '短期入所施設 ひまわり園',
-          ryakusyo: '短期ひまわり園',
-          serviceMeisyoCode: '24',
-          serviceMeisyo: '短期入所',
-          jigyosyoBango: '1121000011',
-          post1: '001',
-          post2: '0012',
-          jyusyo: '●●市××町1-1-1',
-          tel: '',
-          tel1: '',
-          tel2: '',
-          tel3: '',
-          fax: '',
-          fax1: '',
-          fax2: '',
-          fax3: '',
-          mail: '',
-          rese: 0,
-          enabled: true,
-          seikyuDaihyo: 0,
-          seikyuDaihyoDisp: '',
-          tekiouStartDate: '20220304',
-          kyutikubun: 1,
-          takinouJigyo: 0,
-          sisetsusyurui: '福祉施設',
-          teiin: '',
-          takinougataNado: '',
-          jinin: '',
-          jigyosyoku: '',
-          tujyo: '公立',
-          kasan: 'なし',
-          jigyojissi: '',
-          taisyo: 'なし',
-        },
-        {
-          code: '1003',
-          serviceJigyosyoMei: '障害者支援施設 ひまわり園',
-          ryakusyo: '入所ひまわり園',
-          serviceMeisyoCode: '32',
-          serviceMeisyo: '施設入所支援',
-          jigyosyoBango: '1121000011',
-          post1: '001',
-          post2: '0012',
-          jyusyo: '●●市××町1-1-1',
-          tel: '',
-          tel1: '',
-          tel2: '',
-          tel3: '',
-          fax: '',
-          fax1: '',
-          fax2: '',
-          fax3: '',
-          mail: '',
-          rese: 0,
-          enabled: true,
-          seikyuDaihyo: 1,
-          seikyuDaihyoDisp: '〇',
-          tekiouStartDate: '20220304',
-          kyutikubun: 1,
-          takinouJigyo: 0,
-          sisetsusyurui: '',
-          teiin: '21人～40人',
-          takinougataNado: '20人以下',
-          jinin: '',
-          jigyosyoku: '',
-          tujyo: '',
-          kasan: '',
-          jigyojissi: '',
-          taisyo: '',
-        },
-        {
-          code: '1004',
-          serviceJigyosyoMei: '障害者支援施設 たんぽぽ園',
-          ryakusyo: '生活たんぽぽ園',
-          serviceMeisyoCode: '22',
-          serviceMeisyo: '生活介護',
-          jigyosyoBango: '1121000012',
-          post1: '',
-          post2: '',
-          jyusyo: '',
-          tel: '',
-          tel1: '',
-          tel2: '',
-          tel3: '',
-          fax: '',
-          fax1: '',
-          fax2: '',
-          fax3: '',
-          mail: '',
-          rese: 0,
-          enabled: true,
-          seikyuDaihyo: 0,
-          seikyuDaihyoDisp: '',
-          tekiouStartDate: '20220304',
-          kyutikubun: 1,
-          takinouJigyo: 1,
-          sisetsusyurui: '',
-          teiin: '21人～40人',
-          takinougataNado: '20人以下',
-          jinin: 'Ⅱ型(2:1)',
-          jigyosyoku: '指定',
-          tujyo: '',
-          kasan: '',
-          jigyojissi: '本体',
-          taisyo: '',
-        },
-        {
-          code: '1005',
-          serviceJigyosyoMei: '障害者支援施設 たんぽぽ園',
-          ryakusyo: '入所ひまわり園',
-          serviceMeisyoCode: '32',
-          serviceMeisyo: '施設入所支援',
-          jigyosyoBango: '1121000012',
-          post1: '',
-          post2: '',
-          jyusyo: '',
-          tel: '',
-          tel1: '',
-          tel2: '',
-          tel3: '',
-          fax: '',
-          fax1: '',
-          fax2: '',
-          fax3: '',
-          mail: '',
-          rese: 0,
-          enabled: true,
-          seikyuDaihyo: 1,
-          seikyuDaihyoDisp: '',
-          tekiouStartDate: '20220304',
-          kyutikubun: 1,
-          takinouJigyo: 0,
-          sisetsusyurui: '',
-          teiin: '21人～40人',
-          takinougataNado: '',
-          jinin: '',
-          jigyosyoku: '指定',
-          tujyo: '',
-          kasan: '',
-          jigyojissi: '本体',
-          taisyo: '',
-        },
-        {
-          code: '1006',
-          serviceJigyosyoMei: '障害者支援施設 たんぽぽ園',
-          ryakusyo: '自立訓練たんぽぽ園',
-          serviceMeisyoCode: '41',
-          serviceMeisyo: '自立訓練(機能訓練)',
-          jigyosyoBango: '',
-          post1: '',
-          post2: '',
-          jyusyo: '',
-          tel: '',
-          tel1: '',
-          tel2: '',
-          tel3: '',
-          fax: '',
-          fax1: '',
-          fax2: '',
-          fax3: '',
-          mail: '',
-          rese: 0,
-          enabled: true,
-          seikyuDaihyo: 0,
-          seikyuDaihyoDisp: '',
-          tekiouStartDate: '20220304',
-          kyutikubun: 1,
-          takinouJigyo: 2,
-          sisetsusyurui: '',
-          teiin: '',
-          takinougataNado: '',
-          jinin: '',
-          jigyosyoku: '指定',
-          tujyo: '',
-          kasan: '',
-          jigyojissi: '',
-          taisyo: '',
-        },
-        {
-          code: '1007',
-          serviceJigyosyoMei: '就労支援センター たんぽぽ園',
-          ryakusyo: '就労支援たんぽぽ園',
-          serviceMeisyoCode: '43',
-          serviceMeisyo: '就労移行支援',
-          jigyosyoBango: '1121000022',
-          post1: '',
-          post2: '',
-          jyusyo: '',
-          tel: '',
-          tel1: '',
-          tel2: '',
-          tel3: '',
-          fax: '',
-          fax1: '',
-          fax2: '',
-          fax3: '',
-          mail: '',
-          rese: 0,
-          enabled: true,
-          seikyuDaihyo: 1,
-          seikyuDaihyoDisp: '〇',
-          tekiouStartDate: '20220304',
-          kyutikubun: 1,
-          takinouJigyo: 2,
-          sisetsusyurui: '日中サービス支援型',
-          teiin: '21人～40人',
-          takinougataNado: '',
-          jinin: '',
-          jigyosyoku: '指定',
-          tujyo: '',
-          kasan: '',
-          jigyojissi: '',
-          taisyo: '',
-        },
-        {
-          code: '1008',
-          serviceJigyosyoMei: '就労支援センター たんぽぽ園',
-          ryakusyo: '就労支援たんぽぽ園',
-          serviceMeisyoCode: '46',
-          serviceMeisyo: '就労継続支援B型',
-          jigyosyoBango: '1121000023',
-          post1: '',
-          post2: '',
-          jyusyo: '',
-          tel: '',
-          tel1: '',
-          tel2: '',
-          tel3: '',
-          fax: '',
-          fax1: '',
-          fax2: '',
-          fax3: '',
-          mail: '',
-          rese: 0,
-          enabled: false,
-          seikyuDaihyo: 0,
-          seikyuDaihyoDisp: '',
-          tekiouStartDate: '20220304',
-          kyutikubun: 1,
-          takinouJigyo: 2,
-          sisetsusyurui: '',
-          teiin: '21人～40人',
-          takinougataNado: '',
-          jinin: '',
-          jigyosyoku: '指定',
-          tujyo: '',
-          kasan: '',
-          jigyojissi: '',
-          taisyo: '',
-        }
-      );
-*/
 
-      /*
-      for (let i = 0; i < serviceData.length; i++) {
-        // 住所を接続する
-        if (serviceData[i].jyusyo) {
-          serviceData[i]['jyusyoText'] =
-            '〒' +
-            serviceData[i].post1 +
-            '-' +
-            serviceData[i].post2 +
-            '\n' +
-            serviceData[i].jyusyo;
-        } else {
-          serviceData[i]['jyusyojyusyoText'] = '';
-        }
+      return ServiceTeikyo().then((result) => {
+        serviceData = result.seikyu_inf;
+        this.merge = [];
 
-        if (serviceData[i].tel1 && serviceData[i].tel2 && serviceData[i].tel3) {
-          serviceData[i]['tel'] =
-            serviceData[i].tel1 +
-            '-' +
-            serviceData[i].tel2 +
-            '-' +
-            serviceData[i].tel3;
-        } else {
-          serviceData[i]['tel'] = '';
-        }
-
-        if (serviceData[i].fax1 && serviceData[i].fax2 && serviceData[i].fax3) {
-          serviceData[i]['fax'] =
-            serviceData[i].fax1 +
-            '-' +
-            serviceData[i].fax2 +
-            '-' +
-            serviceData[i].fax3;
-        } else {
-          serviceData[i]['fax'] = '';
-        }
-
-        serviceData[i]['reseDisp'] = this.reseType[serviceData[i].rese];
-        serviceData[i]['takinouJigyo'] =
-          this.takinouJigyo[serviceData[i].takinouJigyo];
-
-        let y = moment(serviceData[i].tekiouStartDate).format('YYYY');
-        let m = moment(serviceData[i].tekiouStartDate).format('M') - 1;
-        let d = moment(serviceData[i].tekiouStartDate).format('D');
-        serviceData[i]['tekiouStartDate'] = wijmo.Globalize.format(
-          new Date(y, m, d),
-          'gyy/MM'
-        );
-      }
-*/
-      this.merge = [];
-      this.serviceDataMain = serviceData;
-      this.allDataMain = serviceData;
+        return serviceData;
+      });
     },
   },
 };
