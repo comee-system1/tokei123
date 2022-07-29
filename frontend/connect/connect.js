0f9f853b363f795d0ea98c73acee35fd0df760d1
comee-system <system@comee.jp>
2022-07-29 09:03:19 +0900

    0729

---
+++ frontend/connect/connect.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import common from './Common';
+
+export async function connect(type, uniqid, traceid) {
+    // console.log("connect ok");
+    // console.log("type=>" + type);
+    let str = type.slice(1);
+    return await axios.get(common.LOG_DOMAIN + ":" + common.LOG_PORT + "/" + str + "/" + uniqid + "/" + traceid).then(function (response) {
+        // console.log("connects");
+        //console.log(response.data);
+        return response.data.response;
+    }).catch(function (error) {
+        console.log("api接続用サーバーに接続失敗。BACKENDの実行確認");
+        console.log(error);
+    });
+}
