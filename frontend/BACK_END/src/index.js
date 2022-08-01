2ab7a77fe2de9c55e4902b48d06cf1cea2c284d4
comee-system1 <system@comee.jp>
2022-08-02 00:20:55 +0900

    0802

--- frontend/BACK_END/src/index.js
+++ frontend/BACK_END/src/index.js
@@ -3,6 +3,18 @@ const app = express()
 const log4js = require('log4js');
 const moment = require('moment')
 const date = moment().format('YYYYMMDD');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+app.use(cors({
+    origin: 'http://localhost:8080', //アクセス許可するオリジン
+    credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+    optionsSuccessStatus: 200 //レスポンスstatusを200に設定
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 log4js.configure({
     appenders: {
@@ -64,12 +76,12 @@ app.post('/:name/:uniqid/:trace', (req, res) => {
     // res.header("Access-Control-Allow-Origin: *");
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-    //res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'POST');
     res.header('Access-Control-Allow-Credentials', true);
     //res.header('Access-Control-Max-Age', '86400');
     //res.sendStatus(200);
     console.log("ddddddddddddd");
-    console.log(req);
+    console.log(req.body);
 
     let filename = `${req.params.name}`;
     let uniqid = `${req.params.uniqid}`;
--- frontend/sihmain/connect/postConnect.js
+++ frontend/sihmain/connect/postConnect.js
@@ -2,13 +2,17 @@ import axios from 'axios';
 import common from './Common';
 
 export async function postConnect(type, uniqid, traceid) {
-    console.log("connect ok");
+    console.log("connect ok1");
     console.log("type=>" + type);
     let str = type.slice(1);
-
-    return await axios.post(common.LOG_DOMAIN + ":" + common.LOG_PORT + "/" + str + "/" + uniqid + "/" + traceid).then(function (response) {
+    const data = {
+        firstName: 'firstTest',
+        lastName: 'lastTest'
+    };
+    return await axios.post(common.LOG_DOMAIN + ":" + common.LOG_PORT + "/" + str + "/" + uniqid + "/" + traceid, { data }).then(function (response) {
         // console.log("connects");
         //console.log(response.data);
+        console.log('response body:', response.data);
         return response.data.response;
     }).catch(function (error) {
         console.log("api接続用サーバーに接続失敗。BACKENDの実行確認");
