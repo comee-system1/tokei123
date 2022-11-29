e17637af63948a28957b7f1eddadb09dd9dcc54d
comee-system1 <system@comee.jp>
2022-11-29 10:04:24 +0900

    1129

--- frontend/CERT/keycloak/backend/shell/sample.sh
+++ frontend/CERT/keycloak/backend/shell/sample.sh
@@ -11,4 +11,40 @@ echo $TOKEN
 
 echo "\n"
 echo " * user creation\n"
-curl -v http://localhost:8884/admin/realms/admin99/users -H "Content-Type: application/json" -H "Authorization: bearer $TOKEN"   --data '{"firstName":"xyz","lastName":"xyz", "email":"demo2@gmail.com", "enabled":"true"}'
+# curl -v http://localhost:8884/admin/realms/admin99/users -H "Content-Type: application/json" -H "Authorization: bearer $TOKEN"   --data '{"firstName":"xyz","lastName":"xyz", "email":"demo2@gmail.com", "enabled":"true"}'
+
+curl --insecure -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json; charset=UTF-8" -d '{"username":"hogehoge", "enabled": true, "email": "hogehoge@example.com"}' http://localhost:8884/admin/realms/admin99/users -v
+
+echo "\n"
+echo " * user getInformation \n"
+
+USER=`curl --insecure -X GET -H "Authorization: Bearer $TOKEN" http://localhost:8884/admin/realms/admin99/users?username=hogehoge`
+echo $USER
+
+echo "\n"
+echo " * user getInformation ID \n"
+
+export userid=$(curl --insecure -X GET -H "Authorization: Bearer $TOKEN" http://localhost:8884/admin/realms/admin99/users?username=hogehoge | jq -r .[].id)
+
+echo $userid
+
+echo "\n"
+echo " * user getInformation SETTINT PASSWORD \n"
+
+
+curl --insecure -X PUT -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json; charset=UTF-8" -d '{"type": "password", "value": "test1234", "temporary": true}' http://localhost:8884/admin/realms/admin99/users/${userid}/reset-password -v
+
+
+echo "\n"
+echo " * user getInformation attribute \n"
+
+export attributes=$(curl --insecure -X GET -H "Authorization: Bearer $TOKEN" http://localhost:8884/admin/realms/admin99/users?username=hogehoge | jq -r '.[].attributes.secret_question')
+
+echo $attributes
+
+
+echo "\n"
+echo " * user EditInformation Attribute \n"
+
+curl --insecure -X PUT -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json; charset=UTF-8" -d '{"attributes":{"secret_question":"ffffffffffff"}}' http://localhost:8884/admin/realms/admin99/users/${userid} -v
+
